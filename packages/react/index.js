5c365a0ab28c8a845a52524e2a1154607b64b3ba
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2023-10-12 22:20:59 +0800

    fix(projects):fix  ts,react config

--- packages/react/index.js
+++ packages/react/index.js
@@ -4,6 +4,11 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "rascal-ts",
   ],
+  settings: {
+    react: {
+      version: "detect", //确保ESLint规则与项目中使用的React版本保持一致
+    },
+  },
   rules: {
     "react/react-in-jsx-scope": "off",
   },
